934c8ecc687453ab16fe11585ef2d422f2b8eceb
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-03 20:05:17 -0400

    first commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,6 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
+const router = require("express").Router();
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,7 +25,6 @@
   }
  */
 
-
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -41,7 +40,6 @@
     "message": "Invalid credentials"
   }
  */
-
 
 /**
   3 [GET] /api/auth/logout
@@ -59,5 +57,5 @@
   }
  */
 
- 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
